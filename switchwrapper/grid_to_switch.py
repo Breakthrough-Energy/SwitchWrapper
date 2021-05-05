67474d3bc0a538b2091a22a3f296533e8eb1022c
Daniel Olsen <daniel.olsen@breakthroughenergy.org>
2021-05-04 20:59:12 -0700

    chore: add skeleton for grid_to_switch function

---
+++ switchwrapper/grid_to_switch.py
@@ -0,0 +1,87 @@
+def grid_to_switch(grid, outputfolder):
+    financials_filepath = os.path.join(outputfolder, "financials.csv")
+    build_financials().to_csv(financials_filepath, index=False)
+
+    fuels_filepath = os.path.join(outputfolder, "fuels.csv")
+    build_fuels().to_csv(fuels_filepath, index=False)
+
+    fuel_cost_filepath = os.path.join(outputfolder, "fuel_cost.csv")
+    build_fuel_cost().to_csv(fuel_cost_filepath, index=False)
+
+    generation_projects_info_filepath = os.path.join(
+        outputfolder, "generation_projects_info.csv"
+    )
+    build_generation_projects_info().to_csv(
+        generation_projects_info_filepath, index=False
+    )
+
+    gen_build_costs_filepath = os.path.join(outputfolder, "gen_build_costs.csv")
+    build_gen_build_costs().to_csv(gen_build_costs_filepath, index=False)
+
+    gen_build_predetermined_filepath = os.path.join(
+        outputfolder, "gen_build_predetermined.csv"
+    )
+    build_gen_build_predetermined().to_csv(
+        gen_build_predetermined_filepath, index=False
+    )
+
+    load_zones_filepath = os.path.join(outputfolder, "gen_build_costs.csv")
+    build_load_zones().to_csv(load_zones_filepath, index=False)
+
+    non_fuel_energy_source_filepath = os.path.join(
+        outputfolder, "non_fuel_energy_source.csv"
+    )
+    build_non_fuel_energy_source().to_csv(non_fuel_energy_source_filepath, index=False)
+
+    periods_filepath = os.path.join(outputfolder, "periods.csv")
+    build_periods().to_csv(periods_filepath, index=False)
+
+    transmission_lines_filepath = os.path.join(outputfolder, "transmission_lines.csv")
+    build_transmission_lines().to_csv(transmission_lines_filepath, index=False)
+
+    trans_params_filepath = os.path.join(outputfolder, "trans_params.csv")
+    build_trans_params().to_csv(trans_params_filepath, index=False)
+
+
+def build_financials():
+    pass
+
+
+def build_fuels():
+    pass
+
+
+def build_fuel_cost():
+    pass
+
+
+def build_generation_projects_info():
+    pass
+
+
+def build_gen_build_costs():
+    pass
+
+
+def build_gen_build_predetermined():
+    pass
+
+
+def build_load_zones():
+    pass
+
+
+def build_non_fuel_energy_source():
+    pass
+
+
+def build_periods():
+    pass
+
+
+def build_transmission_lines():
+    pass
+
+
+def build_trans_params():
+    pass
