455b39783c24d88ef8a07d7b2f882cf5d58c8cc4
YifanLi86 <73915007+YifanLi86@users.noreply.github.com>
2021-05-18 14:43:40 -0500

    chore: add entries to fuel_mapping to cover 'dfo' and 'other' types

--- switchwrapper/const.py
+++ switchwrapper/const.py
@@ -3,7 +3,7 @@ financial_parameters = {
     "interest_rate": 0.029,
 }
 
-fuels = ["Coal", "NaturalGas", "Uranium"]
+fuels = ["Coal", "NaturalGas", "Uranium", "Petroleum", "Other"]
 
 fuel_mapping = {
     "wind": "Wind",
@@ -14,6 +14,8 @@ fuel_mapping = {
     "coal": "Coal",
     "ng": "NaturalGas",
     "nuclear": "Uranium",
+    "dfo": "Petroleum",
+    "other": "Other",
 }
 
 load_parameters = {
