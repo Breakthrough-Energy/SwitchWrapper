1f27d6fb894a16d9664c8c3104d1a9466ed7e46f
danielolsen <danielolsen@users.noreply.github.com>
2021-06-17 10:02:35 -0700

    chore: remove setup.cfg, move information to setup.py (#110)

--- setup.py
+++ setup.py
@@ -1,4 +1,16 @@
-import setuptools
+from setuptools import find_packages, setup
 
-if __name__ == "__main__":
-    setuptools.setup()
+setup(
+    name="SwitchWrapper",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "haversine~=2.3",
+        "pandas~=1.2",
+        (
+            "powersimdata "
+            "@ git+https://github.com/Breakthrough-Energy/PowerSimData#egg=PowerSimData"
+        ),
+        "switch-model==2.0.6",
+    ],
+)
