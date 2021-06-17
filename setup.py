831e6543b11bdc63e7105a28cf8119743574be04
Daniel Olsen <daniel.olsen@breakthroughenergy.org>
2021-06-17 13:53:32 -0700

    chore: bump version number to v0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="SwitchWrapper",
-    version="0.1.0",
+    version="0.2.0",
     packages=find_packages(),
     install_requires=[
         "haversine~=2.3",
